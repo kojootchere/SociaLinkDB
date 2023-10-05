d390d45e4453cc7b5ee0878e90ce7c818240dcbf
Kojo Otchere <kojootchere@MacBook-Air-3.local>
2023-10-04 20:22:46 -0400

    Initialized project structure and set up basic controllers for User and Thought

---
+++ controllers/thoughtController.js
@@ -0,0 +1,31 @@
+const Thought = require('../models/Thought');
+
+module.exports = {
+  getAllThoughts(req, res) {
+    // Logic to fetch all thoughts
+  },
+
+  getThoughtById(req, res) {
+    // Logic to fetch a thought by ID
+  },
+
+  createThought(req, res) {
+    // Logic to create a new thought
+  },
+
+  updateThought(req, res) {
+    // Logic to update a thought by ID
+  },
+
+  deleteThought(req, res) {
+    // Logic to delete a thought by ID
+  },
+
+  addReaction(req, res) {
+    // Logic to add a reaction to a thought
+  },
+
+  deleteReaction(req, res) {
+    // Logic to delete a reaction from a thought
+  }
+};
---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const User = require('../models/User');
+const Thought = require('../models/Thought');
+
+module.exports = {
+  getAllUsers(req, res) {
+    // Logic to fetch all users
+  },
+
+  getUserById(req, res) {
+    // Logic to fetch a user by ID
+  },
+
+  createUser(req, res) {
+    // Logic to create a new user
+  },
+
+  updateUser(req, res) {
+    // Logic to update a user by ID
+  },
+
+  deleteUser(req, res) {
+    // Logic to delete a user by ID
+  },
+
+  addFriend(req, res) {
+    // Logic to add a friend to a user's friend list
+  },
+
+  deleteFriend(req, res) {
+    // Logic to delete a friend from a user's friend list
+  }
+};
